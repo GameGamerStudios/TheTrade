fbb681cd177dbbe9e746f872dce6039b3ad58566
GameGamerStudios <c.m.mahaffey-grauer@hotmail.com>
2025-11-19 16:04:44 -0500

    Init

---
+++ src/main/java/com/gamegamerstudios/theTrade/Plugin.java
@@ -0,0 +1,20 @@
+package com.gamegamerstudios.theTrade;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
